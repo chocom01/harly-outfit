04d12a6dfe3c62c177a246f057954886caf54cf0
chocom01 <mukola0101@gmail.com>
2021-07-16 17:14:52 +0300

    new product files

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only: :show
+
+  def index
+    render json: Product.all
+  end
+
+  def show
+    render json: product
+  end
+
+  private
+
+  attr_reader :product
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+    # return status: 404 if @product.nil?
+  end
+end
