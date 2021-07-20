28c35cb2865ce1c9467872e29f18e59abb202468
chocom01 <mukola0101@gmail.com>
2021-07-20 15:30:53 +0300

    fix assotiation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   paginates_per 10
+  has_many :order_items
   has_many :orders, through: :order_items
-  has_many :orders
   has_many :reviews
   has_many :photos, dependent: :destroy
 end
