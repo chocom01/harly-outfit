22ced59b8cdf9c495d8ad7720540080b6de60e0a
chocom01 <mukola0101@gmail.com>
2021-07-21 12:24:49 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: redirect('products')
+  resources :products, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
