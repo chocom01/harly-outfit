a4f31928a11bc6129bcbda518184f83e8f6f5c27
chocom01 <mukola0101@gmail.com>
2021-07-22 18:52:14 +0300

    add enum product for gender

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+
+  enum gender: {
+    Male: 0,
+    Female: 1,
+  }
+
   paginates_per 10
   has_many :order_items
   has_many :orders, through: :order_items
