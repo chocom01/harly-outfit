6a4233b286f4b06fb830753f05fb68349a51bf88
chocom01 <mukola0101@gmail.com>
2021-07-28 15:40:06 +0300

    Little change in routes.rb, schema.rb and seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 Rails.application.routes.draw do
   devise_for :user
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: redirect('products')
+  root to: "products#index"
   resources :products, only: %i[index show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
