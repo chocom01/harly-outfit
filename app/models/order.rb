1fde58ae3004c7e2c23af49c59b2ccc2e418a7e6
chocom01 <mukola0101@gmail.com>
2021-08-05 17:17:51 +0300

    Add pagination, scope "available_cart" and after_touch: sum_products_prices for Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,19 @@ class Order < ApplicationRecord
     paid: 1
   }
 
+  paginates_per 10
   has_many :order_items
   has_many :products, through: :order_items
   belongs_to :user
+
+  scope :available_cart, -> { where(status: 'cart') }
+
+  after_touch :sum_products_price
+
+  private
+
+  def sum_products_price
+    self.sum_cents = products.sum(:price_cents)
+    save
+  end
 end
