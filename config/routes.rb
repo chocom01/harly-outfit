6e05232d79891e58bd428261da9990f127d290ee
chocom01 <mukola0101@gmail.com>
2021-08-05 17:19:59 +0300

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   devise_for :user
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: "products#index"
+  root to: 'products#index'
   resources :products, only: %i[index show]
+  resources :orders, only: %i[index create]
+  get 'orders/add_product', to: 'orders#add_product'
+  get '/orders/show_cart', to: 'orders#show_cart'
 end
