6f0bdb44ab636e63c7d913137758e33f39af2f5e
chocom01 <mukola0101@gmail.com>
2021-08-06 19:12:56 +0300

    Avoid n+1 in orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @orders = Order.page(params[:page])
+    @orders = Order.page(params[:page]).includes(:products)
   end
 
   def show_cart
