17a530037e28fa1f7951dddfda96cb33eda0e922
chocom01 <mukola0101@gmail.com>
2021-08-06 17:49:25 +0300

    Edit OrderItem model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,13 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  after_create :items_count_increment
+  after_touch :items_count_increment
+
+  private
+
+  def items_count_increment
+    OrderItem.increment_counter(:items_count, self.id)
+  end
 end
