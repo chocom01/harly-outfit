dc24a4cde373ebe9ddfe66f0d8797408b9878555
chocom01 <mukola0101@gmail.com>
2021-08-06 17:51:25 +0300

    Beauty changes routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   devise_for :user
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'products#index'
+
   resources :products, only: %i[index show]
-  resources :orders, only: %i[index create]
-  get 'orders/add_product', to: 'orders#add_product'
-  get '/orders/show_cart', to: 'orders#show_cart'
+
+  resources :orders, only: :index do
+    collection do
+      get :add_product
+      get :show_cart
+      get :pay
+    end
+  end
 end
