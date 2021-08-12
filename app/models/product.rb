c4807d99bf0340364b00e5b614cf40ac300c18ef
chocom01 <mukola0101@gmail.com>
2021-08-12 17:32:44 +0300

    Delete useless dependents

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,8 @@ class Product < ApplicationRecord
   }
 
   paginates_per 10
-  has_many :order_items, dependent: :destroy
-  has_many :orders, through: :order_items, dependent: :destroy
+  has_many :order_items
+  has_many :orders, through: :order_items
   has_many :reviews, dependent: :destroy
   has_many :photos, dependent: :destroy
 end
