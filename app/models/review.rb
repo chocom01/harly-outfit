d9c66c349310479eb7c550aaf9470fe781a73f4f
chocom01 <mukola0101@gmail.com>
2021-08-13 18:06:51 +0300

    Add possibility to let users leave reviews for the purchased product

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,18 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validate :rights_to_live_review
+
+  private
+
+  def rights_to_live_review
+    user_bought_product? || errors.add(:review, "user didn't buy this product")
+  end
+
+  def user_bought_product?
+    Product.joins(order_items: :order)
+           .where(id: product_id, orders: { user_id: user_id, status: 'paid' })
+           .exists?
+  end
 end
