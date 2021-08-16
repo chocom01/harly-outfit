2bdfae4e186ee32c2e1b93e979edce5f97fc6d3d
chocom01 <mukola0101@gmail.com>
2021-08-16 13:03:44 +0300

    Add availability column for products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_092157) do
+ActiveRecord::Schema.define(version: 2021_08_16_095839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_092157) do
     t.integer "gender"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "availability", default: 1
   end
 
   create_table "reviews", force: :cascade do |t|
